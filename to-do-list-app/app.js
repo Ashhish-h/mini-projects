951b66caba7a642b38f3bc1cc9a4b4f298e90b14
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-26 21:30:34 +0530

    fix bug

--- to-do-list-app/app.js
+++ to-do-list-app/app.js
@@ -8,12 +8,27 @@ function addTask(){
         let listItem = document.createElement("li");
         listItem.innerHTML = inputBox.value;
         listContainer.appendChild(listItem);
+
+        let span = document.createElement("span");
+        span.innerHTML = '\u00d7';
+        listItem.appendChild(span);
     }
 
     inputBox.value = "";
     saveTask();
 
 }
+
+//to change the state of the task from checked to unchecked
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveTask();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveTask();
+    }
+});
 
 //to store data into the local storage of the browser
 function saveTask(){ 
