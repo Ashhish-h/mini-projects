1d1cac2898a76f0f8c7b6861a1c3f6a2b925d0e1
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-05-27 20:37:57 +0530

    add dynamic number update to slider control

--- random-password-generator/script.js
+++ random-password-generator/script.js
@@ -4,5 +4,25 @@ const passBox = document.getElementById("passBox");
 const lowerCaseElment = document.getElementById("lowercase");
 const upperCaseElment = document.getElementById("uppercase");
 const numberElement = document.getElementById("numbers");
-const symbolelement = document.getElementById("symbols");
+const symbolselement = document.getElementById("symbols");
 const generateBtn = document.getElementById("getBtn")  
+
+const lowercaseLetter = "abcdefghijklmanoprstuvwxyz";
+const uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*<>?/.,\|`~";
+
+
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener("input", () =>{
+    sliderValue.textContent = inputSlider.value;
+});
+
+generateBtn.addEventListener("click", () =>{
+    
+    passBox.value = generatePassword();
+});
+
+function generatePassword(){
+
+}
